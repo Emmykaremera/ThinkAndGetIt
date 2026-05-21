43aa1e75129a1b7d6af25cef0f39093cf137fbbd
emmy <emmy.byiringiro2023@kepler.org>
2026-05-21 10:54:00 +0200

    I added configReader and Endpoints files to avoid hard coding

--- src/main/java/pages/LoginPage.java
+++ src/main/java/pages/LoginPage.java
@@ -1,28 +1,60 @@
+//package pages;
+//
+//import com.microsoft.playwright.Page;
+//
+//public class LoginPage {
+//
+//    private final Page page;
+//
+//    public LoginPage(Page page){
+//        this.page = page;
+//    }
+//
+//    public void login(String username, String password){
+//
+//        page.navigate("https://think-and-get-it-frontend.onrender.com/login");
+//
+//        page.locator("input").first().fill(username);
+//
+//        page.locator("input").nth(1).fill(password);
+//
+//        page.locator("button[type='submit']").click();
+//    }
+//
+//}
+
+
 package pages;
 
 import com.microsoft.playwright.Page;
+import utils.ConfigReader;
+import utils.Endpoints;
 
 public class LoginPage {
 
     private final Page page;
 
     public LoginPage(Page page){
+
         this.page = page;
     }
 
     public void login(String username, String password){
 
-        page.navigate("https://think-and-get-it-frontend.onrender.com/login");
+        page.navigate(
+                ConfigReader.getProperty("base.url")
+                        + Endpoints.LOGIN
+        );
 
-        page.locator("input").first().fill(username);
+        page.locator("input")
+                .first()
+                .fill(username);
 
-        page.locator("input").nth(1).fill(password);
+        page.locator("input")
+                .nth(1)
+                .fill(password);
 
-        page.locator("button[type='submit'], button:has-text('login'), button:has-text('sign')").click();
-    }
-
-    public String getErrorMessage() {
-
-        return page.locator(".error, .alert, .toast").innerText();
+        page.locator("button[type='submit']")
+                .click();
     }
 }
--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -1,12 +1,15 @@
 package base;
-
 import com.microsoft.playwright.*;
 import org.testng.annotations.BeforeClass;
+import utils.ConfigReader;
+import utils.Endpoints;
 
 public class BaseTest {
 
     protected Playwright playwright;
+
     protected Browser browser;
+
     protected Page page;
 
     @BeforeClass
@@ -15,11 +18,16 @@ public class BaseTest {
         playwright = Playwright.create();
 
         browser = playwright.chromium().launch(
-                new BrowserType.LaunchOptions().setHeadless(false)
+
+                new BrowserType.LaunchOptions()
+                        .setHeadless(false)
         );
+
         page = browser.newPage();
-        page.navigate("https://think-and-get-it-frontend.onrender.com/home");
 
-
+        page.navigate(
+                ConfigReader.getProperty("base.url")
+                        + Endpoints.HOME
+        );
     }
 }
